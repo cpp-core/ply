bd110e83022c3d25ec5d89a230eae304e84809bf
Mark Melton <mark.melton@notlem.com>
2019-08-19 12:32:17 -0600

    Added basic Layout test.

---
+++ test/src/test_layout.cpp
@@ -0,0 +1,32 @@
+// Copyright (C) 2019 by Mark Melton
+//
+
+#include <gtest/gtest.h>
+#include "ply/layout.h"
+
+using namespace ply;
+
+TEST(Ply, Layout)
+{
+    EXPECT_EQ(as_string<Layout>(), "layout");
+    
+    auto expected = core::json::object();
+    auto actual = Layout().json;
+    EXPECT_EQ(actual.dump(), expected.dump());
+}
+
+TEST(Ply, LayoutScene)
+{
+    core::json expected = {{ "scene", {{ "xaxis", {{ "visible", false }}}}}};
+    auto actual = Layout(Scene(XAxis(Visible::False))).json;
+    EXPECT_EQ(actual.dump(), expected.dump());
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
+
