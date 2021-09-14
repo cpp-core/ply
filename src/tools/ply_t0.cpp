fda817a5b9e9600df352d84f01e2819f1775aab6
Mark Melton <mark.melton@notlem.com>
2021-09-13 20:52:54 -0400

    Added simple example for scatter plot.

--- src/tools/ply_t0.cpp
+++ src/tools/ply_t0.cpp
@@ -1,10 +1,17 @@
-// Copyright (C) 2019 by Mark Melton
+// Copyright (C) 2019, 2021 by Mark Melton
 //
 
 #include "core/tool.h"
+#include "ply/ply.h"
+
+using namespace ply;
 
 int tool_main(int argc, const char *argv[])
 {
+    reals x = {1.0,2.0,3.0}, y = {4.0,5.0,4.0};
+    Traces traces;
+    traces.emplace_back(Scatter().x(x).y(y));
+    generate_html(traces, Layout());
     return 0;
 }
 
