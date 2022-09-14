097ef173e28f1f2cd73716cb95328e68551fe86d
Mark Melton <mark.melton@notlem.com>
2022-09-14 17:55:52 -0400

    Added test that uses generate which exercises the inja library.

---
+++ test/src/test_generate.cpp
@@ -0,0 +1,26 @@
+// Copyright (C) 2019, 2022 by Mark Melton
+//
+
+#include <gtest/gtest.h>
+#include "ply/ply.h"
+
+using namespace ply;
+
+TEST(Ply, Generate)
+{
+    std::vector<double> data = { 1, 2, 3, 3, 2, 1 };
+    Trace trace{Scatter(Name{"test-data"}).y(data)};
+
+    std::stringstream ss;
+    generate_html({trace}, Layout(), ss);
+    EXPECT_GT(ss.str().size(), 1000);
+}
+
+int main(int argc, char *argv[])
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
+
+
+
